786d035ecac8bf1b48a840075c051f101a2beeea
Matias Gonzalez <tanganillo@gmail.com>
2025-06-28 17:31:54 -0300

    creamos funci{on adios y pusimos comentarios de los comandos de git

--- ejemplo_git.py
+++ ejemplo_git.py
@@ -7,4 +7,19 @@ def hola_comi():
 
 def adios():
         print("adios!)")
+
+
+def adios():
+        print("¡Adios!")
         
+
+#Comandos que utilizamos:
+#git init -> crear el repo, solo una vez
+
+#git add . -> añadir todos los archivos al repo
+
+#git commit -m "mensaje" -> guardar los cambios con un mensaje
+
+#git status -> ver el estado del repo, que archivos han cambiado, las veces que quieran para ver el estado del repo
+
+#git log -> ver el historial de commits, las veces que quieran para ver 
