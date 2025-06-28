4dcde9ecfe5d27acc8d25122690bb0ba417d5aa4
Matias Gonzalez <tanganillo@gmail.com>
2025-06-28 18:41:44 -0300

    restauré el archivo original

--- ejemplo_git.py
+++ ejemplo_git.py
@@ -1,1 +1,31 @@
-print("hola mundo")
+def hola_mundo():
+        print("hola mundo!")
+
+
+def hola_comi():
+        print("hola comi!")
+
+def adios():
+        print("adios!)")
+
+
+def adios():
+        print("¡Adios!")
+        
+
+#Comandos que utilizamos:
+#git init -> crear el repo, solo una vez
+
+#git add . -> añadir todos los archivos al repo
+
+#git commit -m "mensaje" -> guardar los cambios con un mensaje
+
+#git status -> ver el estado del repo, que archivos han cambiado, las veces que quieran para ver el estado del repo
+
+#git log -> ver el historial de commits, las veces que quieran para ver 
+
+#git branch -> nos indica en que rama estamos
+
+#git checkout -b dev -> creamos una rama nueva llamada dev y nos cambiamos a ella 
+
+#git checkout master -> volvimos a la rama principal
